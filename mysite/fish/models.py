086a59a3c7080310a0acfa5f56ceecfc7fd4608e
Matthew <matthewbropez@gmail.com>
2019-06-13 17:58:34 -0400

    Update models.py
    Since I scraped from a new source I had to add another column into the db

--- mysite/fish/models.py
+++ mysite/fish/models.py
@@ -7,6 +7,7 @@ class Fish(models.Model):
     price = models.IntegerField()
     shadow = models.CharField(max_length=255, default='small')
     location = models.CharField(max_length=255, default='river')
+    months_available = models.CharField(max_length=255, default='All year')
     time = models.CharField(max_length=255, default='all day')
 
     def __str__(self):
