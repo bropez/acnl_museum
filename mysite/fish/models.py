a9836af72c848b6eca3ac1da6df8cb1ba5d05364
matthew <matthewbropez@gmail.com>
2019-06-14 13:18:59 -0400

    These were also changed in the merge

--- mysite/fish/models.py
+++ mysite/fish/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.db import models
 
 
@@ -13,19 +12,3 @@ class Fish(models.Model):
 
     def __str__(self):
         return self.name
-=======
-from django.db import models
-
-
-class Fish(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    image = models.CharField(max_length=255)
-    price = models.IntegerField()
-    shadow = models.CharField(max_length=255, default='small')
-    location = models.CharField(max_length=255, default='river')
-    months_available = models.CharField(max_length=255, default='All year')
-    time = models.CharField(max_length=255, default='all day')
-
-    def __str__(self):
-        return self.name
->>>>>>> c4abd3cca37536a0f47e17ec883a310d99abcae9
